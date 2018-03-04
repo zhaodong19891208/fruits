2305651e12925609a06a44d224664e4b6a853e7e
zhaodong19891208 <33253481+zhaodong19891208@users.noreply.github.com>
2018-03-04 23:20:07 +0800

    ..
    ..

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {
-    '/tt':{
+    '/tt':{  
           target: 'https://wap.fruitday.com/',
           pathRewrite: {
             '^/tt': ""
